31192268420f4681bf538fc3d6699c8ce81482cf
Eric Berget <berget3333@gmail.com>
2025-02-04 09:41:48 -0600

    Fix: Configure Vite path aliases properly

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     rollupOptions: {
       external: ['date-fns']
